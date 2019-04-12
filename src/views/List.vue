e869218a46aba615c7a69eda167689ef77fcae85
alexwdd <20779512@qq.com>
2019-04-13 00:03:42 +0800

    0413

--- src/views/List.vue
+++ src/views/List.vue
@@ -15,12 +15,17 @@
 
         <div class="cateList" v-show="cateShow">
             <div class="hd">全部分类 <van-icon name="cross" @click="show"></van-icon></div>
-            <div class="bd">
-                <li v-for="(vo,index) in cate" :key="vo.id" @click="changeCate1(vo.id,index)">{{vo.title}}</li>
-            </div>
+            <swipe class="my-swipe" :auto="0">
+				<swipe-item v-for="vo in quick">
+                    <div class="quick">
+                        <li v-for="f in vo" :key="f.id" @click="changeCate1(f)"><img :src="f.icon"><p>{{f.title}}</p></li>
+                    </div>
+                </swipe-item>
+			</swipe>               
         </div>
 
         <div style="height:92px"></div>
+        
         <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
             <van-row class="news" v-for="vo in info" :key="vo.articleid">
                 <van-col span="8"><div class="img" @click="detail(vo.articleid)"><img v-lazy="vo.thumb_s"></div></van-col>
@@ -49,13 +54,22 @@ import { Lazyload } from 'vant';
 Vue.use(Lazyload,{
     loading:'../static/image/default_320.jpg'
 });
+
+import { Swipe, SwipeItem } from 'vue-swipe';
+import 'vue-swipe/dist/vue-swipe.css';
+//注册轮播图
+Vue.component('swipe', Swipe);
+Vue.component('swipe-item', SwipeItem);
+
 export default {
     data() {
         return {
-            cateShow:false,
+            cateShow:true,
             cateName:'',
             sort:0,
             cate:[{title:'全部',id:0,checked:true}],
+            quick:[],
+            current:0,
             cateActive:1,
             info:[],
             loading: false,
@@ -69,6 +83,8 @@ export default {
             if (to.name == "list") {
                 this.info = [];
                 this.cate = [{title:'全部',id:0,checked:true}],
+                this.quick = [];
+                this.cateShow=false;
                 this.page = 1;
                 this.sort = 0;
                 this.onLoad();
@@ -94,10 +110,10 @@ export default {
             this.page = 1;
             this.onLoad();
         },
-        changeCate1(sort,index){
+        changeCate1(value){
             this.cateShow = false;
-            this.cateActive = index;
-            this.sort = sort;
+            this.cateActive = value.index;
+            this.sort = value.id;
             this.info = [];
             //this.cate = [{title:'全部',id:0,checked:true}],
             this.page = 1;
@@ -125,7 +141,8 @@ export default {
                     if(that.cate.length==1){
                         that.cate = that.cate.concat(res.body.cate);
                     }
-                    that.cateName = res.body.cateName;        
+                    that.cateName = res.body.cateName;
+                    that.quick = res.body.quick;
                     that.page++;          
                     if(res.body.next==0){
                         that.finished = true;
@@ -135,7 +152,10 @@ export default {
                 }
             });
         }
-    }
+    },
+    mounted:function(){
+        this.show();
+    },
 };
 </script>
 <style scoped>
@@ -148,8 +168,17 @@ export default {
 .cateList{position: fixed; top:46px; width: 100%; z-index: 999; background: #fff}
 .cateList .hd{height: 44px; line-height: 44px; text-align: center; position: relative; background: #05c1af; color: #fff}
 .cateList i{position: absolute;right: 5px; top: 15px}
-.cateList .bd{background: #fff; clear: both; overflow: hidden;border-bottom:1px #f1f1f1 solid}
-.cateList .bd li{float: left; width: 25%; text-align: center;font-size: 12px; padding: 10px 0}
+.my-swipe{height: 250px;width: 100vw; border-bottom: 1px #f1f1f1 solid}
+.wrap >>> .mint-swipe-indicator.is-active {background: #f00;}
+.quick{clear: both; overflow: hidden;}
+.quick li{float: left; width:20%; text-align: center;font-size: 12px; padding: 10px 0}
+.quick li img{display: block; margin: auto; height: 40px}
+.custom-indicator {position: absolute;left: 0px;bottom:10px;width: 100%;height: 20px;text-align: center}
+.custom-indicator li{display: inline-block; padding: 0 3px}
+.custom-indicator li span{display: block; width: 6px; height: 6px; border-radius:3px; background: #ccc}
+.custom-indicator li span.active{background: #f60; width: 14px}
+
+
 .news{clear: both; background:#fff; overflow: hidden; display: flex; padding: 10px; border-bottom:1px #dbdbdb dashed}
 .news .img{width: 110px; margin-right: 10px; float: left;}
 .news .img img{width: 100%; height:80px}
@@ -160,4 +189,6 @@ export default {
 .news .info .bottom .price{float: left; font-size: 14px;color: #05c1af}
 .news .info .bottom .date{font-size: 12px; text-align: right; color: #999; line-height: 20px; float: right;}
 .news .info i{color: #05c1af}
+
+
 </style>
