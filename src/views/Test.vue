5d6143447a432819428dbb28c930c1c835bf9787
alexwdd <20779512@qq.com>
2019-04-12 19:14:23 +0800

    0412

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="wrap">
 		<van-nav-bar fixed :title="cateName" left-arrow @click-left="onClickLeft"/>
-
+		<div style="height:46px"></div>
 		<div id="fallBox">
-			<vue-waterfall-easy :imgsArr="imgsArr" @scrollReachBottom="getData">
+			<vue-waterfall-easy :imgsArr="info" @scrollReachBottom="getData"  @click="detail">
 				<template slot-scope="props">
 					<div class="fall">
-						<div class="img"><img :src="props.value.picname"></div>
 						<div class="title">{{props.value.title}}</div>
 						<div class="footer">
 							<div class="date">{{props.value.createTime}}</div>
-							<div class="like">{{props.value.hit}}</div>
+							<div class="like">
+								<van-icon name="like-o" />
+								<div class="like-total">{{props.value.hit}}</div>
+							</div>
 						</div>	
 					</div>
 				</template>
@@ -26,19 +28,37 @@ export default {
 	data() {
 		return {
 			cateName:'',
-			imgsArr: [],
+			info: [],
+			finished: false,
 			page: 1, // request param
 		}
 	},
+	watch: {
+        $route(to) {
+            if (to.name == "test") {
+                this.info = [];
+                this.page = 1;
+                this.finished = false;
+                this.getData();
+            }
+        }
+    },
 	components: {
 		vueWaterfallEasy
 	},
   	methods: {
 		onClickLeft() {
         	this.$router.go(-1);
-        },
+		},
+		detail(event, { index, value }) {
+			this.$router.push({name:'view',params:{ id:value.id }})		
+		},
       	getData() {			
-            var that = this;
+			var that = this;
+			if(that.finished){
+                this.$refs.waterfall.waterfallOver();
+                return false;
+            }
             let data = {
 				cityID : that.config.CITYID,
 				cid:94,
@@ -47,8 +67,14 @@ export default {
             that.$http.post("V3/weixin/news",data).then(result => {
                 let res = result.data;
                 if (res.code == 0) {
-                    // 加载状态结束           
-					that.imgsArr = that.imgsArr.concat(res.body.data);  
+					// 加载状态结束
+					for (let i = 0; i < res.body.data.length; i++) {
+						res.body.data[i]['src'] = res.body.data[i]['picname'];
+					}
+					if(res.body.next==0){
+                        that.finished = true;
+                    }
+					that.info = that.info.concat(res.body.data);  
 					that.cateName = res.body.cateName;
                     that.page++;          
                 }else{
@@ -66,6 +92,12 @@ export default {
 .wrap >>> .van-nav-bar .van-icon {
     color: #05c1af;
 }
-#fallBox{width: 100%; height:800px;}
-
+#fallBox{width: 100%; height:100vh;}
+.fall{clear: both;background: #fff; overflow: hidden; padding: 5px;}
+.fall .title{font-size: 14px;clear: both;color:#000}
+.fall .footer{clear: both;}
+.fall .date{line-height: 30px;float: left;font-size: 12px}
+.fall .like{float: right; line-height: 30px; color: #999}
+.fall .like i{float: left; margin-top: 7px; margin-right: 3px}
+.fall .like .like-total{float: left; font-size: 12px}
 </style>
