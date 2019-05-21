4d2457c2f15108fca9e5422f317d5bdc4d5c81f7
alexwdd <20779512@qq.com>
2019-05-21 17:25:21 +0800

    0521

--- src/views/chat/Index.vue
+++ src/views/chat/Index.vue
@@ -42,8 +42,14 @@
                 <div class="user">
                     <div class="face"><img :src="vo.face"></div>
                     <div class="name"><p>{{vo.nickname}}</p><span>{{vo.createTime}}</span></div>
-                    <div class="focus focused" v-if="vo.focus" @click=doFocus(idx,vo)>已关注</div>
-                    <div class="focus" v-else="" @click=doFocus(idx,vo)>关注</div>
+
+                    <template v-if="user.id == vo.memberID">
+                        <div class="focus focused" @click=doDel(idx,vo)>删除</div>
+                    </template>
+                    <template v-else="">
+                        <div class="arrowBtn" @click="onClickAction(vo)"><van-icon name="arrow-down" /></div>
+                        <div class="focus" v-if="!vo.focus" @click=doFocus(vo)>关注</div>
+                    </template>                    
                 </div>
                 <div class="say" :id="'say'+vo.id"><span class="tag" v-for="tag in vo.tag" :key="tag.name" :style="'color:'+tag.color">#{{tag.name}}#</span>{{vo.content}}</div>
                 <div class="btn" :id="'btn'+vo.id" v-if="vo.content.length>100" @click="openSay(vo.id)">展开</div>
@@ -65,7 +71,7 @@
                 <div class="bottom">
                     <div class="read">{{vo.hit}}阅读</div>
                     <div class="action">
-                        <li @click="doLike(idx,vo)"><i class="icon icon-like"></i> {{vo.like}}</li>
+                        <li @click="doLike(idx,vo)"><i class="icon icon-like" :class="{'active':vo.liked=='1'}"></i> {{vo.like}}</li>
                         <li @click="gotoComment(vo)"><i class="icon icon-wechat"></i> {{vo.comment}}</li>
                         <li><i class="icon icon-share"></i> 分享</li>
                     </div>
@@ -78,6 +84,13 @@
             <li>视频</li>
         </div>
         <div class="ball" @click="showType"><van-icon name="plus" /></div>
+
+        <van-actionsheet
+        v-model="actionShow"
+        :actions="actions"
+        cancel-text="取消"
+        @select="onSelect"
+        />
     </div>
 </template>
 
@@ -87,6 +100,7 @@ import { ImagePreview } from 'vant';
 export default {
     data() {
         return {
+            user:[],
             token:'',
             commentNumber:0,//最新留言数
             replyNumber:0,//最新回复数
@@ -103,7 +117,15 @@ export default {
             finished: false,
             canPost:true,
             cid:0,
-            page:1
+            page:1,
+
+            //选择器
+            local:[],//当前信息
+            actionShow: false,
+            actions: [
+                {name: '关注'},
+                {name: '举报'}
+            ]
         };
     },
     watch: {
@@ -121,11 +143,61 @@ export default {
         }
     },
     created() {        
-        if(user.status==true){
+        if(user.status==true){ 
             this.token = user.token;
+            this.getUserInfo();
         }
     },
     methods: {
+        getUserInfo(){
+            var that = this;
+            if(user.status){
+                var data = {
+                    token:user.token,
+                };                
+                that.$http.post("/V1/chat/userinfo",data).then(result => {
+                    let res = result.data;
+                    this.user = res.body;
+                });
+            }
+        },
+        onClickAction(info){
+            if(info.focus){
+                this.actions[0]['name'] = '取消关注';
+            }else{
+                this.actions[0]['name'] = '关注';
+            }
+            this.local = info;
+            this.actionShow = true;
+        },
+        onSelect(item){//举报、关注选择器
+            this.actionShow = false;
+            if(item.name!='举报'){
+                this.doFocus(this.local);
+            }
+        },
+        doDel(index,info){//删除话题
+            var that = this;
+            that.$dialog.confirm({
+                title: '系统提示',
+                message: '确认删除吗'
+            }).then(() => {
+                var data = {
+                    token:user.token,
+                    id:info.id
+                };                
+                that.$http.post("/V1/chat/del",data).then(result => {
+                    let res = result.data;
+                    if (res.code == 0) {
+                        that.info.splice(index, 1);
+                    }else if(res.code==999){
+                        window.location.href='app://login';  
+                    }else{
+                        that.$dialog.alert({title:'错误信息',message:res.desc});
+                    }
+                });
+            })
+        },
         showType(){
             this.typeShow = !this.typeShow;
         },
@@ -188,8 +260,10 @@ export default {
                         this.$toast(res.desc);
                         if(res.desc=='已点赞'){
                             that.info[index].like++;
+                            that.info[index].liked = 1;
                         }else{
                             that.info[index].like--;
+                            that.info[index].liked = 0;
                         }
                     }else if(res.code==999){
                         window.location.href='app://login';  
@@ -199,7 +273,7 @@ export default {
                 });
             }
         },
-        doFocus(index,info){//关注
+        doFocus(info){//关注
             var that = this;
             if(user.status){
                 var data = {
@@ -210,12 +284,16 @@ export default {
                 that.$http.post("/V1/chat/focus",data).then(result => {
                     let res = result.data;
                     if (res.code == 0) {
-                        this.$toast(res.desc);
-                        if(res.desc=='已关注'){
-                            that.info[index].focus = true;
-                        }else{
-                            that.info[index].focus = false;
-                        }
+                        this.$toast(res.desc);                        
+                        for(var i in that.info){
+                            if(that.info[i]['memberID'] == info.memberID ){
+                                if(res.desc=='已关注'){
+                                    that.info[i].focus = true;
+                                }else{
+                                    that.info[i].focus = false;
+                                }
+                            }
+                        }                            
                     }else if(res.code==999){
                         window.location.href='app://login';  
                     }else{
@@ -320,8 +398,11 @@ export default {
 .chat .user .name{float: left;font-size: 12px; line-height:20px; padding: 5px 0}
 .chat .user .name p{ margin: 0;}
 .chat .user .name span{color: #999}
-.chat .user .focus{float: right; font-size: 12px; height: 24px; line-height: 24px; border-radius: 12px; background-color: #c00; width: 60px; text-align: center; color: #fff; margin-top: 10px}
-.chat .user .focused{background-color: #ccc; color: #fff;}
+.chat .user .focus{float: right; font-size: 12px; height: 24px; line-height: 24px; border-radius: 12px; background-color: #05c1af; width: 60px; text-align: center; color: #fff; margin-top: 10px; margin-right: 10px;}
+.chat .user .focused{border: 1px #dbdbdb solid; background: #fff; color: #999; margin-right: 0}
+
+.chat .user .arrowBtn{float: right; border:1px #dbdbdb solid; width: 22px; height: 22px; border-radius: 50%; text-align: center; margin-top: 10px}
+.chat .user .arrowBtn i{color: #999; font-size: 14px; line-height: 22px}
 .chat .say{clear: both; font-size: 14px; margin-bottom: 10px; padding: 0 10px;overflow: hidden;max-height: 58px}
 .chat .photo{clear: both; padding-left: 10px}
 .chat .photo li{float: left; width: 33.333%; padding-right: 10px; box-sizing: border-box; padding-bottom: 10px}
@@ -332,5 +413,6 @@ export default {
 .chat .bottom .action{float: right;}
 .chat .action li{float: left; text-align: center; font-size: 13px; line-height: 30px; color: #999; padding:0 10px}
 .chat .action li i{font-size: 16px; display: inline;}
+.chat .action li i.active{color: #05c1af}
 .btn{text-align: right; font-size: 14px; padding-right: 10px; color: #586a9c; margin-top: -10px; margin-bottom: 10px}
 </style>
